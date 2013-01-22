3ec4d3d4f66988840338c9f4eddc941ee5b5de84
William Belle <william.belle@gmail.com>
2013-01-22 19:57:47 +0100

    update hdd labels and fix typo

--- launch.sh
+++ launch.sh
@@ -6,7 +6,7 @@ sleep 2
 conky -d -c conky-ubuntu
 conky -d -c conky-gnome
 conky -d -c conky-system
-conky -d -c conky-network-ram
+conky -d -c conky-network
 conky -d -c conky-gpu-sensor-fan
 conky -d -c conky-hdd
 conky -d -c conky-process
