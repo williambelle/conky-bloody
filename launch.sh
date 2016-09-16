2ba038345de15054e720a4402a9cfb88db3261be
William Belle <william.belle@gmail.com>
2016-09-16 20:26:03 +0200

    fix launch script

--- launch.sh
+++ launch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sleep 10
-killall conky 
+killall conky
 sleep 2
 conky -d -c conky-ubuntu
 conky -d -c conky-gnome
 conky -d -c conky-system
 conky -d -c conky-network
-conky -d -c conky-gpu-sensor-fan
-conky -d -c conky-hdd
+conky -d -c conky-gpu-audio-hdd
+conky -d -c conky-fs
 conky -d -c conky-process
 exit
